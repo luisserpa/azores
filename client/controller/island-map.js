6a5f92f2ffa72b8e55c1b31bc0c291bad252d238
Luis Serpa <luis.ff.serpa@hotmail.com>
2018-03-02 15:24:32 -0100

    google map is already showing with json

--- client/controller/island-map.js
+++ client/controller/island-map.js
@@ -1,22 +1,29 @@
 import json from "../json/historic-moments.js";
 
-/*
 Template.islandMap.rendered=function(){
-
-  console.log("JSON FILE: ",json);
-
-    json.forEach(function(element){
-      console.log("ELEMENT: "+element);
-      Session.set("jsonMarkers",element);
-    });
-
+  Session.set("mapJSON",json);
 }
-*/
 
 Template.islandMap.helpers({
+
   markers(){
-    console.log("JSON FILE: ", json);
-    return json;
+    return function initMap() {
+
+          var uluru = {lat: 60.700, lng: 2.100};
+          var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 5,
+            center: uluru
+          });
+
+          Session.get("mapJSON").forEach(function(element){
+            var marker = new google.maps.Marker({
+              position: {lat: element.lat, lng: element.lng},
+              map: map
+            });
+            console.log("ELEMENT: ",element);
+          });
+
+        }
   }
 
 });
