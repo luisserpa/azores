9a8fd92788ba326e44c6673c4f658c4415ce1f6c
Luis Serpa <luis.ff.serpa@hotmail.com>
2018-03-13 22:39:48 -0100

    working on translations

--- client/controller/header.js
+++ client/controller/header.js
@@ -35,7 +35,6 @@ Template.header.events({
 
 Template.displayUserName.helpers({
     showName() {
-        console.log("USER NAME: ", Session.get("sessionUser").displayName);
         return Session.get("sessionUser").displayName;
     }
 });
