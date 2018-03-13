90fcf96c10d462ceba349b0d50ef507b03949650
SofiaMelo <sofiamelo300@gmail.com>
2018-03-13 21:48:31 -0100

    admin view: delete places and add places

--- client/controller/admin.js
+++ client/controller/admin.js
@@ -1,26 +1,79 @@
 import { Meteor } from 'meteor/meteor';
 import { Places } from "/import/lib/collections/database";
 import { Template } from 'meteor/templating';
+import { ReactiveVar } from "meteor/reactive-var";
 
+let clickedAddPlace;
 
 Template.admin.onCreated(function() {
     Meteor.subscribe('placesList');
+    clickedAddPlace = new ReactiveVar(false);
 });
 
 Template.admin.helpers({
     places() {
-     return Places.find({});
+    return Places.find({});
+    },
+
+    showAddPlace() {
+    return clickedAddPlace.get();
     }
-})
+});
 
 Template.place.onCreated(function() {
     console.log(this);
-})
+});
+
+Template.admin.events({
+    'click .addPlace'() {
+        clickedAddPlace.set(true);
+    }
+});
 
 Template.place.events({
     'submit #delete'(event) {
         event.preventDefault();
-        console.log(event);
-        console.log(event.target.mongoId.value); 
+        var confirmDelete = confirm("You want to delete " + '\"' +
+         Places.findOne({ _id: event.target.mongoId.value }).en.title 
+         + '\"' + " from database?");
+        if (confirmDelete) {
+            Meteor.call("deletePlace", event.target.mongoId.value);
+        }
     }
-})
+});
+
+Template.newPlace.events({
+    'submit #createPlace'(event) {
+        event.preventDefault();
+        var placeData = {
+            pt: {
+                title: event.target.titlePt.value,
+                description: event.target.summaryPt.value,
+                visitHours: event.target.hoursPt.value,
+                longDescription: event.target.descPt.value
+            },
+            en: {
+                title: event.target.titleEn.value,
+                description: event.target.summaryEn.value,
+                visitHours: event.target.hoursEn.value,
+                longDescription: event.target.descEn.value
+            },
+            type: event.target.category.value,
+            rating: [],
+            usersVoted: [],
+            comments: [],
+            lat: event.target.latitude.value,
+            lng: event.target.longitude.value,
+            image_1: event.target.img1.value,
+            image_2: event.target.img2.value,
+        };
+
+        Meteor.call("findByNamePt", event.target.titlePt.value, function(err, result) {
+            if (!result) {
+                Meteor.call("addPlace", placeData);
+                event.target.reset();
+                clickedAddPlace.set(false);
+            }
+        });
+    }
+});
